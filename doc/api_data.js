fe89234e452311679fd2a840ebc9b99aff36b99f
Kritsanasak Kuntaros <roboconk@gmail.com>
2015-01-16 20:58:54 +0700

    Add API Doc

---
+++ doc/api_data.js
@@ -0,0 +1,284 @@
+define({ "api": [
+  {
+    "type": "post",
+    "url": "/event/gallery/:id",
+    "title": "POST /event/gallery/:id",
+    "description": "<p>Save picture after first picture</p> ",
+    "name": "PostAddEventGallery",
+    "group": "Event",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Event id</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "picture",
+            "description": "<p>Picture in base64_encode</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "user_id",
+            "description": "<p>User id</p> "
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/EventCTL.php",
+    "groupTitle": "Event"
+  },
+  {
+    "type": "post",
+    "url": "/event/gallery",
+    "title": "POST /event/gallery",
+    "description": "<p>Booking event with first picture</p> ",
+    "name": "PostEventGallery",
+    "group": "Event",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "picture",
+            "description": "<p>Picture in base64_encode</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "user_id",
+            "description": "<p>User id</p> "
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/EventCTL.php",
+    "groupTitle": "Event"
+  },
+  {
+    "type": "put",
+    "url": "/event/:id",
+    "title": "PUT /event/:id",
+    "description": "<p>Update event details</p> ",
+    "name": "PutEvent",
+    "group": "Event",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Event id</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>Event name</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "Text",
+            "optional": false,
+            "field": "detail",
+            "description": "<p>Event description</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "date_start",
+            "description": "<p>Event datetime E.g. 2014-01-15 11:00:00</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "date_end",
+            "description": "<p>Event datetime</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "credit",
+            "description": "<p>Something where are you get this event from</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "user_id",
+            "description": "<p>User id</p> "
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/EventCTL.php",
+    "groupTitle": "Event"
+  },
+  {
+    "type": "post",
+    "url": "/oauth/facebook",
+    "title": "POST /oauth/facebook",
+    "description": "<p>Register with facebook</p> ",
+    "name": "OauthFacebook",
+    "group": "OAuth",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "facebook_token",
+            "description": ""
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "ios_device_token",
+            "description": "<p>Token from your mobile</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "android_token",
+            "description": "<p>Token from your mobile</p> "
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n \"user_id\": \"54506d34da354df2078b4569\",\n \"access_token\": \"9f0f853517eaaed3c0b74838e6e95693\",\n \"type\": \"normal\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/OAuthCTL.php",
+    "groupTitle": "OAuth"
+  },
+  {
+    "type": "get",
+    "url": "/sniff/category",
+    "title": "GET /sniff/category",
+    "description": "<p>Get all category</p> ",
+    "name": "SniffCategory",
+    "group": "Sniff",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n     \"length\": 19,\n     \"data\": [\n         {\n             \"id\": \"6f2da37e72bf9e100b40567c\",\n             \"name\" \"Awards\",\n         },\n         {\n             \"id\": \"e9c5c932c205770da433d3de\",\n             \"name\" \"Conferences\",\n         },\n         ...\n     ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/SniffCTL.php",
+    "groupTitle": "Sniff"
+  },
+  {
+    "type": "post",
+    "url": "/register",
+    "title": "POST /register",
+    "description": "<p>Register with email</p> ",
+    "name": "Register",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "username",
+            "description": "<p>Your username using for login to system</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Email address</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Your password</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "gender",
+            "description": "<p>male or female</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "birth_date",
+            "description": "<p>Your birth date</p> "
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./private/src/Main/CTL/RegisterCTL.php",
+    "groupTitle": "User"
+  },
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p> "
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./doc/main.js",
+    "group": "_home_robocon_pla2_eventsniff_doc_main_js",
+    "groupTitle": "_home_robocon_pla2_eventsniff_doc_main_js",
+    "name": ""
+  }
+] });
---
+++ doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-01-16T08:46:26.498Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.1"
+  }
+});
