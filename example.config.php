c54a5604108154d967d72131932672f1297512bf
Kritsanasak Kuntaros <roboconk@gmail.com>
2015-06-11 19:14:18 +0700

    Update example config file

--- example.config.php
+++ example.config.php
@@ -3,8 +3,11 @@ define('SITE_PRODUCTION', true);
 
 define('SITE_BLOWFISH', ''); /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 define('MEDIA_HOST', '127.0.0.1');
+define('MONGO_USER', '');
+define('MONGO_PASS', '');
 define('MONGO_HOST', '127.0.0.1');
 define('MONGO_PORT', '27017');
+define('MONGO_DB', '');
 
 define('FB_APP_ID', 'YOUR_FACEBOOK_APP_ID');
 define('FB_APP_SECRET', 'YOUR_FACEBOOK_APP_SECRET');
